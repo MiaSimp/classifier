8229a6e59384b0f5a731cb507cd45d1416e35c3c
MiaSimp <153345142+MiaSimp@users.noreply.github.com>
2023-12-13 15:34:07 -0500

    Update project3.py

--- project3.py
+++ project3.py
@@ -110,23 +110,17 @@ testData = newDF.head(twenPerc)
 # create a new dataframe with the last 80% of the data
 trainData = newDF.tail(eightyPerc)
 
-print(testData)
-print(trainData)
 #reset the indexes of the testing dataset
 trainData.reset_index(drop=True, inplace=True)
 
 # read in stopwords
 f = open("stopwords.txt", encoding="utf8")
-
 stri= ""    
 for line in f:
     stri+=line 
-
 # split the string of stopwords and convert it into list
 x = stri.split()   
-
 f.close()
-
 # declare set of stopwords and update it with the txt file stopwords
 stopwords = nltk.corpus.stopwords.words('english')
 stopwords = set(STOPWORDS)
